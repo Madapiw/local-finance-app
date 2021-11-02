aa666c4bf2e88cf43e32f944a94ec149b13c29ac
Adam Piwonski <01161135@pw.edu.pl>
2021-11-02 12:32:37 +0100

    gitignore add& fixes

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,19 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const Main = require('./routes/main')
+const mysql = require('mysql')
 
-const host = "0.0.0.0";
 const port = 3000
 const app = express();
 
-mongoose.connect('mongodb://27017/todo-app', {
-    useNewUrlParser: true,
-    keepAlive: true,
-    useUnifiedTopology: true,
-    createIndexes: true
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'dbuser',
+    password: 'db123',
+    database: 'finance_app'
 })
 
+connection.connect();
+
 app.use(express.json())
-app.use(Main)
 app.listen(port, host, () => {
     console.log(`Server running on ${host} : ${port}`);
 });
