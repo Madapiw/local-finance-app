d7674face2ac4e58bd7578121c9a27e143e36bc5
Adam Piwoński <88802068+Madapiw@users.noreply.github.com>
2022-03-24 10:11:52 +0100

    server express-gererate

--- backend/middleware/middleware.js
+++
@@ -1,9 +0,0 @@
-function Logs(req, res, next) {
-    const method = req.method;
-    const url = req.url;
-    const ip = req.ip;
-    console.log(`${method} ${url} -- ${ip}`);
-    next();
-};
-
-module.exports = { Logs };
